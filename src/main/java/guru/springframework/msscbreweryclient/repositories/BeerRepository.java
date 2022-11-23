383d40e5c35f813abc2f7ee4ba516fe464afa1d5
Norvald Espedal <norvald.espedal@lyse.net>
2022-11-23 21:04:39 +0100

    Lecture 59, add repository

---
+++ src/main/java/guru/springframework/msscbreweryclient/repositories/BeerRepository.java
@@ -0,0 +1,12 @@
+package guru.springframework.msscbreweryclient.repositories;
+
+import guru.springframework.msscbreweryclient.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+
+}
